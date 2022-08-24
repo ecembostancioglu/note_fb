4894ba62209435ac778a4513ab15f54b3244ca4c
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-08-24 16:07:30 +0300

    Added googleSignIn package

--- lib/authentication/widgets/login_widget.dart
+++ lib/authentication/widgets/login_widget.dart
@@ -1,5 +1,3 @@
-
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todo_fb/authentication/service/auth_service.dart';
@@ -56,14 +54,31 @@ class _LoginWidgetState extends State<LoginWidget> {
             SizedBox(height: 20.h),
             ElevatedButton.icon(
                 style: ElevatedButton.styleFrom(
-                    minimumSize:const Size.fromHeight(50)
+                    minimumSize:const Size.fromHeight(50),
+                    elevation: 6,
                 ),
                 icon:const Icon(Icons.lock_open,size: 30),
                 onPressed: (){
                   _authService.signInAnonymously();
                 },
                 label:const Text('Sign In',
-                    style: TextStyle(fontSize: 24)))
+                    style: TextStyle(fontSize: 24))),
+            SizedBox(height: 20.h),
+            Text('OR',
+                style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20.h),
+            ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  minimumSize:const Size.fromHeight(50),
+                  elevation: 6,
+                  primary: Colors.white70
+                ),
+                icon:Image.asset('assets/images/google.png',height: 30),
+                onPressed: (){
+                  _authService.signInAnonymously();
+                },
+                label:Text('Sign In with Google',
+                    style: TextStyle(fontSize: 24,color: Colors.black54))),
           ],
         ),
       ),
