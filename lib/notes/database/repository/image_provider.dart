fd9b5ed9e90c60152ac29b4178bb2cd4fe59966c
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-10-02 01:44:35 +0300

    Convert an image to base64

--- lib/notes/database/repository/image_provider.dart
+++ lib/notes/database/repository/image_provider.dart
@@ -1,7 +1,20 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ImageProvider with ChangeNotifier{
   String imageKey = 'IMG_KEY';
   SharedPreferences? sharedPreferences;
+
+  void imageToBase64(Uint8List imageBytes) {
+    sharedPreferences!.setString(imageKey,base64.encode(imageBytes));
+  }
+
+  Uint8List base64ToImage(){
+    var data = sharedPreferences!.getString(imageKey);
+    return base64.decode(data!);
+  }
+
 }
