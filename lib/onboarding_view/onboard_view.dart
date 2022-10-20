7a66fcfb53689561a8400f37d7cfe46d4ba41a08
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-10-21 02:14:16 +0300

    Created dot indicator widget

--- lib/onboarding_view/onboard_view.dart
+++ lib/onboarding_view/onboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todo_fb/onboarding_view/widgets/dot_indicator.dart';
 import 'package:todo_fb/onboarding_view/widgets/onboard_content.dart';
 import 'models/onboard.dart';
 
@@ -12,6 +13,7 @@ class OnBoardView extends StatefulWidget {
 
 class _OnBoardViewState extends State<OnBoardView> {
   late PageController _pageController;
+  int _pageIndex=0;
 
   @override
   void initState() {
@@ -29,42 +31,62 @@ class _OnBoardViewState extends State<OnBoardView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: PageView.builder(
-                itemCount:contents.length,
-                controller: _pageController,
-                itemBuilder: (context,index)=>
-                    OnboardContent(
-                        image:contents[index].image,
-                        title:contents[index].title,
-                        description:contents[index].description),
-              ),
-            ),
-            SizedBox(
-              height: 50.h,
-              width: 50.w,
-              child: Padding(
-                padding:EdgeInsets.only(bottom:5.h),
-                child: ElevatedButton(
-                  onPressed: (){
-                    _pageController.nextPage(
-                        duration:const Duration(milliseconds:400),
-                        curve: Curves.ease);
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView.builder(
+                  itemCount:contents.length,
+                  controller: _pageController,
+                  onPageChanged: (index){
+                    setState(() {
+                      _pageIndex=index;
+                    });
                   },
-                  style: ElevatedButton.styleFrom(
-                    shape:const CircleBorder(),
-                      elevation: 10,
-                  ),
-                  child:const Icon(Icons.arrow_forward),
+                  itemBuilder: (context,index)=>
+                      OnboardContent(
+                          image:contents[index].image,
+                          title:contents[index].title,
+                          description:contents[index].description),
                 ),
               ),
-            )
-          ],
+              Row(
+                children: [
+                  ...List.generate(contents.length,
+                          (index) => Padding(
+                            padding: const EdgeInsets.only(right: 4),
+                            child: DotIndicator(isActive: index==_pageIndex),
+
+                          )),
+                  const Spacer(),
+                  SizedBox(
+                    height: 50.h,
+                    width: 50.w,
+                    child: Padding(
+                      padding:EdgeInsets.only(bottom:5.h),
+                      child: ElevatedButton(
+                        onPressed: (){
+                          _pageController.nextPage(
+                              duration:const Duration(milliseconds:400),
+                              curve: Curves.ease);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape:const CircleBorder(),
+                            elevation: 10,
+                        ),
+                        child:const Icon(Icons.arrow_forward),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
 
   }
 }
+
